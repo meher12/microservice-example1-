0637ffd5be53c9af9f805d5cb0adf2180b15e6df
meher12 <meher.khemiri@gmail.com>
2022-08-13 12:26:48 +0100

    Getting started with Circuit Breaker - Resilience4j

---
+++ micro-currency-exchange-service/src/main/java/net/guru/microservice/controller/CircuitBreakerController.java
@@ -0,0 +1,14 @@
+package net.guru.microservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+    
+    @GetMapping("/sample-api")
+    public String sampleApi() {
+        return "Sample Api";
+    }
+
+}
